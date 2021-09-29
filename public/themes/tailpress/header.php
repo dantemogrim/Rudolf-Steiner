9ae6e38f93b68c1a505e3dd2f13eaf79cd1a676a
Dante Mogrim <1991.mogrim@gmail.com>
2021-09-29 14:16:35 +0200

    Page templates wip.

--- public/themes/tailpress/header.php
+++ public/themes/tailpress/header.php
@@ -1,18 +1,15 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" href="<?= get_theme_file_uri('assets/app.css') ?>">
-	<?php wp_head(); ?>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?= get_theme_file_uri('assets/app.css') ?>">
+  <?php wp_head(); ?>
 </head>
-
-<?php $menuItems = wp_get_nav_menu_items('menu-bar'); ?>
-
 <body class="bg-gradient-to-r from-blue-300 via-blue-400 to-blue-500">
-
+<?php wp_body_open(); ?>
+<?php $menuItems = wp_get_nav_menu_items('menu-bar'); ?>
 <header>
-
     <nav class="bg-white flex items-center justify-between flex-wrap p-6">
   <div class="flex items-center flex-shrink-0 text-black mr-6">
     <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
@@ -25,23 +22,12 @@
   </div>
   <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
     <div class="text-sm lg:flex-grow">
-
     <?php foreach ($menuItems as $menuItem) : ?>
-
       <a href="<?= $menuItem->url; ?>" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-pink-500 mr-4">
-
         <?= $menuItem->title; ?>
-
       </a>
-
     <?php endforeach; ?>
-
     </div>
-
   </div>
 </nav>
-
-
-
-
 </header>
