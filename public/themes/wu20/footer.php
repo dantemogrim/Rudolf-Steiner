f2d3903177caecd9f7cc464853551f5d088e0a64
Dante Mogrim <1991.mogrim@gmail.com>
2021-10-07 20:36:02 +0200

    Updated color palette and update nav menu.

--- public/themes/wu20/footer.php
+++ public/themes/wu20/footer.php
@@ -4,20 +4,20 @@
 <?php wp_footer(); ?>
 
 <footer>
-    <h1 class="footer-name">RUDOLF STEINERSKOLAN</h1>
+    <h1 class="footerTextGreen footer-name">RUDOLF STEINERSKOLAN</h1>
     <div class="contact">
         <ul class="footer-adress">
-            <li><b>Adress</b></li>
+            <li class="footerTextGreen"><b>Adress</b></li>
             <li>Rudolf Steinerskolan i Göteborg</li>
             <li>Tallhöjdsgatan 1</li>
             <li>416 74 GÖTEBORG</li>
         </ul>
         <ul class="footer-email">
-            <li><b>E-post</b></li>
+            <li class="footerTextGreen"><b>E-post</b></li>
             <li>rudolf@steinerskolan.se</li>
         </ul>
         <ul class="footer-phone">
-            <li><b>Telefon</b></li>
+            <li class="footerTextGreen"><b>Telefon</b></li>
             <li>031-21 46 32</li>
         </ul>
     </div>
--- public/themes/wu20/header.php
+++ public/themes/wu20/header.php
@@ -13,51 +13,58 @@
     <?php $menuItems = get_menu('header-menu'); ?>
     <div class="mobile-menu">
 
-
         <nav>
             <a href="<?= get_home_url(); ?>">
                 <img class="logo" src="<?= get_template_directory_uri(); ?>/icons/logoAndText.svg" />
             </a>
 
-            <?php foreach ($menuItems as $menuItem) : ?>
-                <?php if (sizeof($menuItem->children) === 0) : ?>
-                    <!-- Single links. -->
-                    <a class="menuLinks mobileMenu" href="<?= $menuItem->url; ?>">
-                        <?= $menuItem->title; ?>
-                    </a>
-                <?php else : ?>
-                    <!-- Parent links. -->
-                    <div class="parentAndChildrenWrapper">
-                        <div class="caretLinkWrapper">
-                            <img class="caret" src="<?= get_template_directory_uri(); ?>/icons/caret-down.svg" />
-                            <a class="menuLinks mobileMenu" href="<?= $menuItem->url; ?>">
-                                <?= $menuItem->title; ?>
-                            </a>
+            <div class="navFlexDesktop">
+
+                <?php foreach ($menuItems as $menuItem) : ?>
+                    <?php if (sizeof($menuItem->children) === 0) : ?>
+                        <!-- Single links. -->
+                        <a class="menuLinks mobileMenu" href="<?= $menuItem->url; ?>">
+                            <?= $menuItem->title; ?>
+                        </a>
+                    <?php else : ?>
+                        <!-- Parent links. -->
+                        <div class="parentAndChildrenWrapper">
+                            <div class="caretLinkWrapper">
+                                <img class="caret" src="<?= get_template_directory_uri(); ?>/icons/caret-down.svg" />
+                                <a class="menuLinks mobileMenu" href="<?= $menuItem->url; ?>">
+                                    <?= $menuItem->title; ?>
+                                </a>
+                            </div>
+                            <ul class="subMenuBox">
+                                <?php foreach ($menuItem->children as $child) : ?>
+                                    <!-- Children links. -->
+                                    <li>
+                                        <a class="childLinks mobileMenu" href="<?= $child->url ?>">
+                                            <?= $child->title; ?>
+                                        </a>
+                                    </li>
+                                <?php endforeach; ?>
+                            </ul>
                         </div>
-                        <ul class="subMenuBox">
-                            <?php foreach ($menuItem->children as $child) : ?>
-                                <!-- Children links. -->
-                                <li>
-                                    <a class="childLinks mobileMenu" href="<?= $child->url ?>">
-                                        <?= $child->title; ?>
-                                    </a>
-                                </li>
-                            <?php endforeach; ?>
-                        </ul>
-                    </div>
-                <?php endif; ?>
-            <?php endforeach; ?>
+                    <?php endif; ?>
+                <?php endforeach; ?>
 
-            <button class="loginButton" type="button">Intranät</button>
-            <button class="applyButton" type="button">Ansök</button>
 
-            <button class="hamburgerButton">
-                <img class="hamburgerIcon" alt="Open mobile navigation." src="<?= get_template_directory_uri(); ?>/icons/list.svg" />
-            </button>
+                <button class="applyButton" type="button">
+                    Ansök
+                    <img class="applyIcon" src="">
+                </button>
 
-            <a class="closeMenuButton">
-                <img class="closeMenuIcon" alt="Close mobile navigation." src="<?= get_template_directory_uri(); ?>/icons/x.svg" />
-            </a>
+                <button class="hamburgerButton">
+                    <img class="hamburgerIcon" alt="Open mobile navigation." src="<?= get_template_directory_uri(); ?>/icons/list.svg" />
+                </button>
+
+                <a class="closeMenuButton">
+                    <img class="closeMenuIcon" alt="Close mobile navigation." src="<?= get_template_directory_uri(); ?>/icons/x.svg" />
+                </a>
+            </div>
 
         </nav>
+
+
         <main>
