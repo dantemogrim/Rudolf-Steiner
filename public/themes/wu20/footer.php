4a99ee04c1b5cb0c257cc848456ac0e207d6374f
Dante Mogrim <1991.mogrim@gmail.com>
2021-10-14 10:59:17 +0200

    Color and font structuring. Removed side bleed on front page.

--- public/themes/wu20/footer.php
+++ public/themes/wu20/footer.php
@@ -6,17 +6,17 @@
     <div class="contact">
         <ul class="footer-adress">
             <li class="footerTextOrange"><b>Adress</b></li>
-            <li>Rudolf Steinerskolan i Göteborg</li>
-            <li>Tallhöjdsgatan 1</li>
-            <li>416 74 GÖTEBORG</li>
+            <li class="white">Rudolf Steinerskolan i Göteborg</li>
+            <li class="white">Tallhöjdsgatan 1</li>
+            <li class="white">416 74 GÖTEBORG</li>
         </ul>
         <ul class="footer-email">
             <li class="footerTextOrange"><b>E-post</b></li>
-            <li>rudolf@steinerskolan.se</li>
+            <li class="white">rudolf@steinerskolan.se</li>
         </ul>
         <ul class="footer-phone">
             <li class="footerTextOrange"><b>Telefon</b></li>
-            <li>031-21 46 32</li>
+            <li class="white">031-21 46 32</li>
         </ul>
     </div>
     <div class="bottom">
